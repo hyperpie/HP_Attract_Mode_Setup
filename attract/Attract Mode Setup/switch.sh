355037c939f14062cc35816dceef606110d6e7e1
Mik <haggistech@gmail.com>
2017-10-03 19:58:56 +0100

    Added header into script to alert as to not official script

--- attract/Attract Mode Setup/switch.sh
+++ attract/Attract Mode Setup/switch.sh
@@ -1,3 +1,14 @@
+#!/usr/bin/env bash
+
+## 
+#
+# This file is NOT part of The RetroPie Project
+#
+# This script is a third party script to switch between Nested and 
+# Simple wheel displays on a RetroPie build.
+#
+##
+
 mode=$(head -n1 ~/.attract/attract.cfg)
 echo $mode
 
